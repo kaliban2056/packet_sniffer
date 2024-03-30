50672b3b4750533e9cdf98d02cb8bb7a157b8fe8
kaliban2056 <margiottasamuel533@gmail.com>
2024-03-30 15:50:26 +0100

    ipv4_packet and ipv4 fn

--- src/main.py
+++ src/main.py
@@ -27,7 +27,16 @@ def get_mac_addr(bytes_addr):
 
 # Unpacks IPv4 packet
 def ipv4_packet(data):
-    
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, taget = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(taget), data[header_length:]
+
+
+# Return properly formatted IPv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
 
 
 main()
